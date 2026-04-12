fae6c09ce684d132e80fdb3ca5f8353ba386e559
YousungYeh <145213418+yousungyeh@users.noreply.github.com>
2026-04-13 01:28:44 +0800

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.py
@@ -0,0 +1,30 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """ 
+        ===== Sol 3 =====
+        ===== Concept =====
+        Change to tuple practice
+        """
+        # stage 1: save a nums_to_idx array.
+        l = len(nums)
+        nums_with_idx = [(nums[i], i) for i in range(l)]
+        nums_with_idx = sorted(nums_with_idx)
+
+        # stage 2: start traverse.
+        i = 0
+        j = len(nums_with_idx)-1
+        for k in range(l):
+            tmp_sum = nums_with_idx[i][0] + nums_with_idx[j][0]
+            if tmp_sum == target:
+                if nums_with_idx[i][1] > nums_with_idx[j][1]:
+                    return [nums_with_idx[j][1], nums_with_idx[i][1]]
+                return [nums_with_idx[i][1], nums_with_idx[j][1]]
+            elif tmp_sum > target:
+                j = j-1
+            else:
+                i = i+1
+        return []
+
+
+
+        
