f9d90384efc04eb7b9861c9f16628bc40f14fe7b
YousungYeh <145213418+yousungyeh@users.noreply.github.com>
2026-04-15 18:09:01 +0800

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # count the {"char": freq} in sliding window
+        count = {}
+        # return max_len
+        res = 0
+        # left (start) of the sliding windows
+        l = 0
+        # max freq char in s (not in sliding window)
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+            
+            # sliding window
+            while (r - l + 1) - maxf > k:
+                # move sliding window
+                count[s[l]] -= 1
+                l += 1
+            # update max_len
+            res = max(res, r - l + 1)
+
+        return res
