fcc4af6e1f96a7dba1da9ee35cf3e38e14bb842f
YousungYeh <145213418+yousungyeh@users.noreply.github.com>
2026-06-15 19:11:02 +0800

    Add: longest-consecutive-sequence - submission-24

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-24.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        Set
+        use O(1) to find item in set or not.
+        """
+        if not nums:
+            return 0
+
+        set_nums = set(nums)
+        max_len = 0
+
+        for num in set_nums:
+            # identify it is start_num or not
+            if num - 1 not in set_nums:
+                current = num # start_num
+                tmp_len = 1
+
+                # start start_num to traverse nums to find its consective seq.
+                while current + 1 in set_nums:
+                    current += 1
+                    tmp_len += 1
+
+                max_len = max(max_len, tmp_len)
+
+        return max_len
