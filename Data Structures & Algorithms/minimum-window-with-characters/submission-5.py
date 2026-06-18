86934487b770e27f1f4a0ce21c2dcf440e8e03d0
YousungYeh <145213418+yousungyeh@users.noreply.github.com>
2026-06-18 16:33:09 +0800

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # print(self.isInclude(Counter(s),Counter(t)))
+        # return ""
+        if len(s) < len(t):
+            return ""
+        ans = ""
+        l = len(s)
+        r = 0
+        l = 0
+        cnt_tmp_str = Counter()
+        cnt_t = Counter(t)
+        for c in s:
+            l += 1
+            cnt_tmp_str[c] += 1
+            while self.isInclude(cnt_tmp_str, cnt_t):
+                # update min_length(ans)
+                tmp_ans = s[r:l]
+                if len(tmp_ans) < len(ans) or not ans:
+                    ans = tmp_ans
+                cnt_tmp_str[s[r]] -= 1
+                r += 1
+        return ans
+
+    def isInclude(self, cnt_s: Counter, cnt_t: Counter) -> bool:
+        for key, val in cnt_t.items():
+            if key not in cnt_s:
+                return False
+            elif cnt_s[key] < cnt_t[key]:
+                return False
+            else:
+                continue
+        return True
+        
